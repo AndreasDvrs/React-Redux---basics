38f8009e73eedb2fbe46653d9d75cc4ff02832ba
Andreas Davros <andreasdavros@gmail.com>
2020-02-15 02:05:35 +0200

    complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useSelector, useDispatch} from 'react-redux';
+
+import {increment} from './actions';
+import {decrement} from './actions';
+
 
 function App() {
+  const counter = useSelector(state => state.myCount);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter {counter} </h1>
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      {isLogged ? <h3>Valuable info not to see if not logged in</h3> : 'NOT logged in'}
+      
     </div>
   );
 }
